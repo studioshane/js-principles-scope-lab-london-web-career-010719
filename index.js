5df560fe6f47fb276839d7c9d724deb3c117f321
studioshane <studioshane@gmail.com>
2019-02-16 20:07:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastFavoriteCustomer = "Shane"
+
+function upperCaseCustomerName(){
+  customerName =  customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+
+  bestCustomer = 'not bob'
+
+}
+function overwriteBestCustomer(){
+
+  bestCustomer = 'maybe bob'
+
+}
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'andy'
+}
+
